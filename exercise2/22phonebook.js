05998237bc39a6db64164060aab90cfb29746a78
lightweight-baby <73614271+lightweight-baby@users.noreply.github.com>
2020-11-21 17:44:32 +0200

    updated phonebook
    includes error handlers and success messages

--- exercise2/22phonebook.js
+++ exercise2/22phonebook.js
@@ -1,11 +1,55 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import contactService from './services/22servercom'
+
+const Notification = (props) => {
+  //notfication takes two props, namely the message that will be displayed
+  //and a boolean which determines whether its an error message or not
+  const notifStyle = {
+    fontSize: 20,
+    margin: 20,
+    padding: 10,
+    borderRadius: 10,
+    borderStyle: 'solid',
+    borderColor: 'green',
+  }
+  
+  const message = props.arr[0]
+  const err = props.arr[1]
+
+  if (message === null) {
+    return null
+  }
+
+  //change output style if error
+  if (err) {
+    notifStyle.borderColor = 'red'
+  }
+
+  return (
+    <div style={notifStyle}>
+      {message}
+    </div>
+  )
+}
 
 
 const Searchbox = (props) => {
   const search = props.search
-  const handleSearch = props.handleSearch
+  const setSearch = props.setSearch
+  const setShowAll = props.setShowAll
   
+  const handleSearch = (event) => {
+    setSearch(event.target.value.toLowerCase())
+    
+    //if input exists, filter
+    if (event.target.value) {
+      setShowAll(false)
+    } //otherwise show all names
+    else {
+      setShowAll(true)
+    }
+  }
+
   return (
     <div>filter phone book by name: 
       <input
@@ -16,12 +60,70 @@ const Searchbox = (props) => {
 }
 
 const PersonForm = (props) => {
-  const handleName = props.handleName
-  const handlePhone = props.handlePhone
-  const addName = props.addName
-  const newName = props.newName
-  const newPhone = props.newPhone
+  const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
+  const persons = props.persons
+  const setPersons = props.setPersons
+  const setNotification = props.setNotification
   
+  const handleName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handlePhone = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const vanish = () => {
+    setTimeout(() => {
+      setNotification([null, false])
+    } ,5000)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    //check whether name already exists in persons
+    const arr = persons.filter(person => person.name === newName)
+
+    //if it doesn't exist, add name
+    if(!arr.length) {
+      const newEntry = {
+        name: newName,
+        number : newPhone,
+        id: persons[persons.length-1].id + 1,
+      }
+      //add name to db
+      contactService
+      .create(newEntry)
+      .then(
+        //add name to state array
+        setPersons(persons.concat(newEntry))
+      )
+      setNotification([`'${newName}' created.`, false])
+      vanish()
+      
+    } //otherwise, alert message and update db
+    else {
+      if(window.confirm(`Replace ${newName}'s phone number with a new one?`)) {
+        const changedEntry = {...arr[0], number:newPhone}
+        contactService
+        .update(arr[0].id, changedEntry)
+        .then(updatedNotes => {
+          setPersons(persons.map(x => x.id !== arr[0].id? x : updatedNotes))
+          setNotification([`'${newName}' updated.`, false])
+          vanish()
+        })
+        .catch(error => {
+          console.log(error)
+          setNotification([`failed to update '${newName}'.`, true])
+          vanish()
+        })
+        
+      }
+    }
+  }
+
   return (
     <form onSubmit={addName}>
       <div>
@@ -42,83 +144,78 @@ const PersonForm = (props) => {
   )
 }
 
-const Namelist = ({persons}) => (
-  persons.map(object => <div key={object.id}>{object.name} {object.number}</div>)
-)
+const Namelist = (props) => {
+  const setNotification = props.setNotification
+  const setPersons = props.setPersons
+  const persons = props.persons
+
+  const confirmation = (name) => (
+    window.confirm(`Are you sure you wanna delete ${name}?`)
+  )
+  
+  const vanish = () => {
+    setTimeout(() => {
+      setNotification([null, false])
+    } ,5000)
+  }
+
+  const handleDelete = (identifier, name) => {
+    if (confirmation(name)) {
+      //delete entry
+      contactService
+      .del(identifier)
+      .then(() => {
+        //delete entry from state array
+        setPersons(persons.filter(x => x.id !== identifier))
+        setNotification([`'${name}' deleted.`, false])
+        vanish()
+      })
+      .catch(error => {
+        console.log(error)
+        setNotification([`failed to delete '${name}'.`, true])
+        vanish()
+      })
+    }
+  }
+  
+  return (
+    props.personsToShow.map(object => <div key={object.id}>
+      {object.name} {object.number}
+      <button onClick={() => handleDelete(object.id, object.name)}>delete</button>
+    </div>)
+  )
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
+  const [ persons, setPersons ] = useState([])
   const [ showAll, setShowAll ] = useState(true)
   const [ search, setSearch ] = useState('')
+  const [ notification, setNotification ] = useState([null, false])
 
   //fetch data from server db.json
   const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    contactService
+      .getAll()
+      .then(updatedAll => {
+        setPersons(updatedAll)
       })
   }
-  
   useEffect(hook, [])
 
-
-  const addName = (event) => {
-    event.preventDefault()
-    
-    //check whether name already exists in persons
-    const arr = persons.filter(person => person.name === newName)
-
-    //it doesn't exist, add name
-    if(!arr.length) {
-      const newEntry = {
-        name: newName,
-        number : newPhone,
-        id: persons.length + 1,
-      }
-      setPersons(persons.concat(newEntry))
-    } //otherwise, alert message
-    else {
-      window.alert(`${newName} is already added to phonebook`)
-    }
-    
-  }
-
-  const handleName = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handlePhone = (event) => {
-    setNewPhone(event.target.value)
-  }
-
-  const handleSearch = (event) => {
-    setSearch(event.target.value.toLowerCase())
-    
-    //if input exists, filter
-    if (event.target.value) {
-      setShowAll(false)
-    } //otherwise show all names
-    else {
-      setShowAll(true)
-    }
-  }
-
   //personstoshow is the whole array if showall is true, and filtered otherwise
-  const personsToShow = showAll
-    ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(search))
+  const personsToShow = showAll? persons
+  : persons.filter(person => person.name.toLowerCase().includes(search))
+  
 
   return (
     <div>
+      <Notification arr={notification} />
       <h2>Search</h2>
-      <Searchbox search={search} handleSearch={handleSearch}/>
+      <Searchbox setShowAll={setShowAll} search={search} setSearch={setSearch}/>
       <h2>Add a new contact</h2>
-      <PersonForm handleName={handleName} handlePhone={handlePhone} addName={addName} newName={newName} newPhone={newPhone}/>
+      <PersonForm persons={persons} setPersons={setPersons} setNotification={setNotification}/>
       <h2>Numbers</h2>
-      <Namelist persons={personsToShow}/>
+      <Namelist personsToShow={personsToShow} persons={persons} setPersons={setPersons} setNotification={setNotification}/>
     </div>
   )
 }
