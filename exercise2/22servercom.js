36c0dc8123a1b274004ef18ebedfad34e3e9cf64
lightweight-baby <73614271+lightweight-baby@users.noreply.github.com>
2020-11-21 17:46:34 +0200

    22servercom handles db communication
    updated some returns to make .then(...) in the 22phonebook file easier to read

---
+++ exercise2/22servercom.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const del = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update,
+  del: del 
+}
